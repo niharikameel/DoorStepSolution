2912b8ea0628b545129748e961f6c3f6c96369c6
RAJA VISHNU RAO CHITNENI <rajavishnurao@users.noreply.github.com>
2016-12-09 18:33:55 +0100

    Signup and sign in link

--- app.js
+++ app.js
@@ -7,44 +7,31 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var routes = require('./routes');
 var bcrypt = require('bcryptjs');
-var flash = require('connect-flash')
+//var bcrypt = require('bcrypt');
+var flash = require('connect-flash');
+var passport = require('passport');
 var passport = require('passport');
 var LocalStrategy = require('passport-local'),Strategy;
 var expressValidator = require('express-validator');
-
-var Sequelize = require('sequelize');
-var connection = new Sequelize('doorstepsolutions', 'root', 'qwerty12345',{
-  dialect: 'mysql'
-});
-
-var User = connection.define('user', {
-  first_name: Sequelize.STRING,
-  last_name: Sequelize.STRING,
-  email: Sequelize.STRING,
-  password: Sequelize.STRING,
-  DOB: Sequelize.DATE,
-  gender: Sequelize.STRING
-}
-);
-
-connection.sync();
+var application = require('./routes/application');
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-app.set('port', 1327);
+var User = require('./models/user').User;
+app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser());
-
 app.use('/img',express.static(path.join(__dirname, 'public/images')));
 app.use('/js',express.static(path.join(__dirname, 'public/javascripts')));
 app.use('/css',express.static(path.join(__dirname, 'public/stylesheets')));
 app.use(session({
   secret:'secret',
-  saveUninitialized:true,
-  resave:true
+  resave: false,
+  saveUninitialized: false
 }));
 app.use(flash());
 app.use(passport.initialize());
@@ -57,10 +44,10 @@ app.all(function(req,res,next){
 });
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
- 
+    var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+
     while(namespace.length) {
       formParam += '[' + namespace.shift() + ']';
     }
@@ -71,16 +58,42 @@ app.use(expressValidator({
     };
   }
 }));
- 
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ where: {email: username} }).then(function(user) {
+        passwd= user ? user.password :''
+        isMatch = User.validPassword(password, passwd,done,user)
+      });
+    }
+));
 
 app.get('/',routes.home);
-app.post('/login',urlencodedParser,routes.login)
-app.post('/signup', urlencodedParser, routes.signup)
-app.get('/home_maintance',routes.home_maintance)
-app.get('/child_care',routes.child_care)
-app.get('/cooking',routes.cooking)
-app.get('/elder_care',routes.elder_care)
-app.get('/tutors',routes.tutors)
-app.get('/event_planner',routes.event_planner)
-app.listen(process.env.PORT || 1327);
+app.post('/login',
+    passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/',
+      failureFlash: true
+    })
+);
+
+app.post('/createservice',routes.createservice)
+app.post('/signup', routes.signup)
+app.get('/home_maintance',application.IsAuthenticated,routes.home_maintance)
+app.get('/child_care',application.IsAuthenticated,routes.child_care)
+app.get('/cooking',application.IsAuthenticated,routes.cooking)
+app.get('/elder_care',application.IsAuthenticated,routes.elder_care)
+app.get('/tutors',application.IsAuthenticated,routes.tutors)
+app.get('/event_planner',application.IsAuthenticated,routes.event_planner)
+app.get('/signout',application.destroySession)
+app.get('/admin',routes.admin)
+app.listen(process.env.PORT || 3000);
 console.log('Express server listening on port ' + app.get('port'));
